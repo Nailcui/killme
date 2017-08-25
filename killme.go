8ffb2dece744dc0ad0049aaa26ddb4bb43af1f28
快雪 <kuaixue@kuaixuedeMacBook-Pro.local>
2017-08-25 18:54:01 +0800

    add README.md and main file killme.go

---
+++ killme.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const fileName string = "killme.sh"
+
+func init() {
+	pid := os.Getpid()
+	f, _ := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0766)
+	content := fmt.Sprintf("#!/bin/bash\nkill -9 %d", pid)
+	f.WriteString(content)
+	f.Close()
+}
